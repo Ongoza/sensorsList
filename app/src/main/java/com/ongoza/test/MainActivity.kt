1140cae91fca8f5bdd01cbdc8702a28a1085f764
oleg@ongoza.com <oleg@ongoza.com>
2019-06-09 14:14:05 +0300

    modified:   app/src/main/java/com/ongoza/test/MainActivity.kt

--- app/src/main/java/com/ongoza/test/MainActivity.kt
+++ app/src/main/java/com/ongoza/test/MainActivity.kt
@@ -6,32 +6,27 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
 import android.widget.TextView
-import androidx.core.text.HtmlCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var listView: TextView = findViewById(R.id.MainText) as TextView
-        var gyroView: TextView = findViewById(R.id.GyroText) as TextView
+        val listView: TextView = findViewById(R.id.MainText) as TextView
+        val gyroView: TextView = findViewById(R.id.GyroText) as TextView
         val mSensorManager: SensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val deviceSensors: List<Sensor> = mSensorManager.getSensorList(Sensor.TYPE_ALL)
-        Log.v ("Total sensors","ww4"+deviceSensors.size)
-        var sb = StringBuilder()
-        var trGyro = true
+        val sb = StringBuilder()
+        var trGyro = false
         sb.append("Sensors list:\n")
         deviceSensors.forEach{
             sb.append(it.stringType.toString()+"\n")
             if(it.stringType.equals("android.sensor.gyroscope")){
                 trGyro = true
             }
-            Log.v("SensorDec",it.stringType)
           }
         val strList = sb.toString()
-        Log.v("SensorDec",strList)
         listView.text = strList
         if(trGyro){
             gyroView.text = Html.fromHtml("<b>Gyroscope <font color='#22AA22'>is present</font></b> on this device")
